233a039af5b5bef8aad1f67f237556866b6ac3e5
Satyam7255 <104812881+Satyam7255@users.noreply.github.com>
2022-05-04 15:43:51 +0530

    Create for_loop.cpp

---
+++ assignment_2/for_loop.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+int a,b;
+cin >> a >> b;
+string Arr1[9] = {"one","two","three","four","five","six","seven","eight","nine"};
+for(int i =a ; i <=b;i++)
+{
+if(i <= 9)
+{
+cout << Arr1[i-1] << endl;
+}
+else
+{
+if(i%2 ==0)
+{
+cout << "even" << endl;
+}
+else
+{
+cout << "odd" << endl;
+}
+}
+}
+return 0;
+}
