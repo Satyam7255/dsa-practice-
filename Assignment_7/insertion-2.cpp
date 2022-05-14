6a5d3559d431f9a8c3069914823db28610b280ca
Satyam7255 <104812881+Satyam7255@users.noreply.github.com>
2022-05-15 00:11:01 +0530

    Create insertion-2.cpp

---
+++ Assignment_7/insertion-2.cpp
@@ -0,0 +1,28 @@
+void insertionSort2(int n, vector<int> arr) {
+    int  j,a;
+     n = arr.size();
+   
+    
+    for(int i=1;i<n;i++)
+    {
+         a = arr[i];
+         j=i-1;
+        while (j>=0 && arr[j]>a)
+        {
+            arr[j+1]=arr[j];
+ 
+            j--;
+        }    
+        arr[j+1]=a;
+        
+        for (int i=0;i<n;i++)
+        {
+        cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+      
+    }
+    
+
+
+}
