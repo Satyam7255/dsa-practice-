6797cd88ecf2b781958087081a838af7decfd888
Satyam7255 <104812881+Satyam7255@users.noreply.github.com>
2022-05-16 00:44:24 +0530

    Create selection.cpp

---
+++ Assignment_7.1/selection.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums)
+    {
+        int n=nums.size();
+
+ 
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(nums[j]<nums[i])
+			{
+				int temp=nums[j];
+				nums[j]=nums[i];
+				nums[i]=temp;
+			}
+		}
+	}
+ 
+    return nums;    
+        
+    }
+};
