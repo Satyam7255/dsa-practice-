5665106450d9212727aac584b0be53c6f0252621
Satyam7255 <104812881+Satyam7255@users.noreply.github.com>
2022-06-23 23:27:02 +0530

    Create Present-Row-column.cpp

---
+++ A-17/Present-Row-column.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ bool isTargetPresentin2d(vector<vector<int> >A, int target)
+ {
+    for(int r=0;r<A.size();r++){
+        for(int c=0;c<A[0].size();c++){
+            if(A[r][c]==target)
+                return true;
+        }
+    }
+     return false;
+ }
+
+bool isTragetPresentInRow(vector<vector<int> >A, int target, int r)
+{
+    //returns true if target is present in row r of A then returns true else return false;
+
+      for(int c=0;c<A[0].size();c++)   
+      {
+         if(A[r-1][c]==target){
+         	 return true;
+         }
+     }
+ 
+     return false;
+}
+ 
+ 
+bool isTragetPresentInCol(vector<vector<int> >A, int target, int c)
+{
+    //returns true if target is present in row r of A then returns true else return false;
+
+
+    for(int r=0;r<A.size();r++)   
+      {
+         if(A[r][c-1]==target)
+         {
+         	      return true;
+         }
+         
+         return true;
+       }
+    return false;
+}
+
+
+
+int main()
+{
+ 
+ 
+ vector<vector<int>>A = { {7,2,12},{9,5,3},{4,10,8}};
+ 
+ 
+ bool a = isTargetPresentin2d(A,4);
+    if (a==0)
+    {
+    	cout<<"not found.."<<endl;
+    }
+    else
+    {
+    	cout<<"found.."<<endl;
+    }
+
+
+ bool b = isTragetPresentInRow(A,5,2); 
+    if (b==0)
+    {
+    	cout<<"not found.."<<endl;
+    }
+    else
+    {
+    	cout<<"found.."<<endl;
+    }
+    
+    
+ bool c = isTragetPresentInCol(A,7,2);
+    if (c==0)
+    {
+    	cout<<"not found.."<<endl;
+    }
+    else
+    {
+    	cout<<"found.."<<endl;
+    }
+    
+ 
+    return 0;
+}
