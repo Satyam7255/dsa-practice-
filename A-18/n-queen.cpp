f4b1ead9c9f003daadbce40f5f3e39db38a947df
Satyam7255 <104812881+Satyam7255@users.noreply.github.com>
2022-07-04 12:04:20 +0530

    Update n-queen.cpp

--- A-18/n-queen.cpp
+++ A-18/n-queen.cpp
@@ -1,34 +1,39 @@
 class Solution {
+    
 public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string> > res;
-        vector<string> nQueens(n,string(n, '.'));
-        solveNQueens(res, nQueens, 0, n);
-        return res;
+    vector<vector<string>>ans;
+    bool isValid(vector<string>board, int row, int col){
+        for(int i=row;i>=0;i--){
+            if(board[i][col]=='Q')
+                return false;
+        }
+        for(int i =row, j= col; i>=0 && j>=0; i--, j--){
+            if(board[i][j]=='Q')
+                return false;
+        }
+        for(int i=row, j=col; i>=0 && j<board.size(); i--, j++){
+            if(board[i][j]=='Q')
+                return false;
+        }
+        return true;
+        
     }
-private:
-    void solveNQueens(vector<vector<string>> &res,vector<string> &nQueens, int row, int &n) {
-        if (row == n) {
-            res.push_back(nQueens);
+    void recursion(vector<string>board, int n, int row){
+        if(row == n){
+            ans.push_back(board);
             return;
         }
-        for (int col = 0; col != n; ++col)
-            if (isValid(nQueens, row, col, n)) {
-                nQueens[row][col] = 'Q';
-                solveNQueens(res, nQueens, row + 1, n);
-                nQueens[row][col] = '.';
+        for(int i=0;i<n;i++){
+            if(isValid(board, row, i)){
+                board[row][i] = 'Q';
+                recursion(board, n, row+1);
+                board[row][i] = '.';
             }
+        }
     }
-    bool isValid(vector<string> &nQueens, int row, int col, int &n) {
-        for (int i = 0; i != row; ++i)
-            if (nQueens[i][col] == 'Q')
-                return false;
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
-            if (nQueens[i][j] == 'Q')
-                return false;
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
-            if (nQueens[i][j] == 'Q')
-                return false;
-        return true;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n, string(n, '.'));
+        recursion(board, n, 0);
+        return ans;
     }
 };
