afbcb6ed807003b28627e982aab09a82b685a515
Satyam7255 <104812881+Satyam7255@users.noreply.github.com>
2022-07-19 00:55:46 +0530

    Create middle of the linked list.cpp

---
+++ A-12/middle of the linked list.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* a = head;
+        ListNode* b = head;
+        while(b!= NULL && b->next!= NULL ) { 
+            a = a->next;
+            b = b->next->next;
+        }
+        return a;
+    }
+};
