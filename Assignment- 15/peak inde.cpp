1d5cffdf0911d4927095a44ae8060c594a984429
Satyam7255 <104812881+Satyam7255@users.noreply.github.com>
2022-09-18 09:31:42 +0530

    Create peak inde.cpp

---
+++ Assignment- 15/peak inde.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) {
+         int low = 1, high = A.size()-2;
+        while(low<high)
+        {
+            int mid = low+(high-low)/2;
+            if(A[mid]<A[mid+1]) 
+                low=mid+1;
+            else 
+                high=mid;
+        }
+        return low;
+    }
+};
